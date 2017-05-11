aba25409c5ddda70c76718829a1c4231f6aa1a2d
iOSmiling <kunlongxue@gmail.com>
2017-05-11 09:19:21 +0800

    fix:networking

--- ShonemeNetWorkingModule/ShonemeNetWorkingModule/XMNetworking/XMNetworkingService.h
+++ ShonemeNetWorkingModule/ShonemeNetWorkingModule/XMNetworking/XMNetworkingService.h
@@ -15,4 +15,16 @@
  */
 + (XMNetworkingService *)sharedService;
 
+
+/**
+ 调用远程接口通用方法
+
+ @param url 接口api
+ @param params 参数
+ @param success 数据请求成功
+ @param code    数据请求失败
+ @param failure 系统请求api失败
+ */
+- (void)connectRemoteUrl:(NSString *)url withParameters:(NSDictionary *)params success:(void (^)(id JSON))success codeFailure:(void (^)(id JSON))code failure:(void (^)(NSString *errorMsg))failure;
+
 @end
--- ShonemeNetWorkingModule/ShonemeNetWorkingModule/XMNetworking/XMNetworkingService.m
+++ ShonemeNetWorkingModule/ShonemeNetWorkingModule/XMNetworking/XMNetworkingService.m
@@ -21,4 +21,71 @@
     return networkService;
 }
 
+- (void)connectRemoteUrl:(NSString *)url withParameters:(NSDictionary *)params success:(void (^)(id JSON))success codeFailure:(void (^)(id JSON))code failure:(void (^)(NSString *errorMsg))failure
+{
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/html", @"text/plain", nil];
+    manager.requestSerializer.timeoutInterval = 30;
+    [manager POST:url parameters:params progress:^(NSProgress * _Nonnull uploadProgress)
+     {
+         
+     } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject)
+     {
+         // 获取服务器响应数据
+         NSString *responseStr =  [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+         NSDictionary *JsonDic = [XMNetworkingService dictionaryWithJsonString:responseStr];
+         
+         NSLog(@"\n url：%@,\n params:%@,\n JsonDic:%@",url,params,JsonDic);
+         
+         if ([JsonDic isKindOfClass:[NSDictionary class]])
+         {
+             if ([JsonDic[@"result"] isEqualToString:@"success"])
+             {
+                 success(JsonDic);
+                 
+             }else if ([JsonDic[@"result"] isEqualToString:@"fail"])
+             {
+                 code(JsonDic);
+             }
+         }
+         
+     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+         
+         NSLog(@"Error: %@", error);
+         NSString *errorMsg;
+         if ([error.domain isEqualToString:@"AFNetworkingErrorDomain"] || [error.domain isEqualToString:@"NSURLErrorDomain"])
+         {
+             errorMsg = @"您已断开网络连接!";
+         }else
+         {
+             errorMsg = @"系统异常";
+         }
+         failure(errorMsg);
+     }];
+
+}
+
+/* json
+ * @brief 把格式化的JSON格式的字符串转换成字典
+ * @param jsonString JSON格式的字符串
+ * @return 返回字典
+ */
++ (NSDictionary *)dictionaryWithJsonString:(NSString *)jsonString
+{
+    if (jsonString == nil)
+    {
+        return nil;
+    }
+    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *error;
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&error];
+    if(error)
+    {
+        return nil;
+    }
+    return dic;
+}
+
+
 @end
